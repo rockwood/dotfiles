9ab3e2b3ae1d06fc03b8934d27da592e1e7035de
Kevin Rockwood <kevin@rockwood.me>
2017-07-11 20:15:38 +0900

    Update bootstrap script to symlink instead of copy
    Hat tip to: https://github.com/alrra/dotfiles/blob/master/src/os/create_symbolic_links.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,89 @@
-#!/usr/bin/env bash
-cd "$(dirname "${BASH_SOURCE}")"
-git pull origin master
-git submodule update --init
+#!/bin/bash
 
-function doIt() {
-  rsync --exclude ".git/" --exclude ".gitmodules" --exclude "script/" --exclude ".DS_Store" \
-    --exclude "bootstrap.sh" --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-  source ~/.bash_profile
+# this symlinks all the dotfiles (and .vim/) to ~/
+# it also symlinks ~/bin for easy updating
+
+# this is safe to run multiple times and will prompt you about anything unclear
+
+answer_is_yes() {
+    [[ "$REPLY" =~ ^[Yy]$ ]] \
+        && return 0 \
+        || return 1
 }
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt
-else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
-  fi
-fi
-unset doIt
+
+ask_for_confirmation() {
+    print_question "$1 (y/n) "
+    read -n 1
+    printf "\n"
+}
+
+execute() {
+    $1 &> /dev/null
+    print_result $? "${2:-$1}"
+}
+
+print_error() {
+    # Print output in red
+    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
+
+print_info() {
+    # Print output in purple
+    printf "\n\e[0;35m $1\e[0m\n\n"
+}
+
+print_question() {
+    # Print output in yellow
+    printf "\e[0;33m  [?] $1\e[0m"
+}
+
+print_result() {
+    [ $1 -eq 0 ] \
+        && print_success "$2" \
+        || print_error "$2"
+
+    [ "$3" == "true" ] && [ $1 -ne 0 ] \
+        && exit
+}
+
+print_success() {
+    # Print output in green
+    printf "\e[0;32m  [✔] $1\e[0m\n"
+}
+
+#
+# Actual symlink stuff
+#
+
+# finds all .dotfiles in this folder
+declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git | sed -e 's|//|/|' | sed -e 's|./.|.|')
+FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim bin .config/fish" # Add extra binaries
+
+main() {
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+
+    for i in ${FILES_TO_SYMLINK[@]}; do
+        sourceFile="$(pwd)/$i"
+        targetFile="$HOME/$i"
+
+        if [ -e "$targetFile" ]; then
+            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+                    rm -rf "$targetFile"
+                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+            else
+                print_success "$targetFile → $sourceFile"
+            fi
+        else
+            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        fi
+    done
+}
+
+main
