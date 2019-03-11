65da4393420deee5662d926a64c2a2c3a56a122f
Kevin Rockwood <kevin@rockwood.me>
2019-03-11 09:14:31 +0900

    Set GO_PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 source ~/.asdf/asdf.fish
 set -x ERL_AFLAGS "-kernel shell_history enabled"
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -x GOPATH "/Users/rockwood/devel/go"
 set -g fish_key_bindings fish_vi_key_bindings
