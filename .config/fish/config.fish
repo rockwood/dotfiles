81542c841fc4d6a63413f46923e318ef8cadb6d7
Kevin Rockwood <kevin@rockwood.me>
2020-05-04 08:06:54 +0900

    Ditch fish vi mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,3 @@ source ~/.asdf/asdf.fish
 set -x ERL_AFLAGS "-kernel shell_history enabled"
 set -x HOMEBREW_EDITOR "vim"
 set -x GOPATH "/Users/rockwood/devel/go"
-set -g fish_key_bindings fish_vi_key_bindings
