f64c9c6623f8b28e37e81d5fa1479633f4faf730
Kevin Rockwood <kevin@rockwood.me>
2022-03-09 22:01:18 -0600

    Use homebrew asdf path for fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-source ~/.asdf/asdf.fish
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
 set -x ERL_AFLAGS "-kernel shell_history enabled"
 set -x HOMEBREW_EDITOR "vim"
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
