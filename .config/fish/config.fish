99675529dff5c236f0a44f9dbb466cc9f889449e
Kevin Rockwood <kevin@rockwood.me>
2022-03-09 22:00:58 -0600

    Drop go path from fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 source ~/.asdf/asdf.fish
 set -x ERL_AFLAGS "-kernel shell_history enabled"
 set -x HOMEBREW_EDITOR "vim"
-set -x GOPATH "/Users/rockwood/devel/go"
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
