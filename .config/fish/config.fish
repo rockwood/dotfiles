7df9578fc0b328651f7ed3a30f2ae3e621179fee
Kevin Rockwood <kevin@rockwood.me>
2022-04-14 10:10:50 +0900

    Disable fish greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+set fish_greeting
 set -x ERL_AFLAGS "-kernel shell_history enabled"
 set -x HOMEBREW_EDITOR "vim"
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
